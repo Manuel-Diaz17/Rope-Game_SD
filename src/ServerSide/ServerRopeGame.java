38006fd5a52ff238c7c6028684891467eae809e2
Manuel Diaz <manu.guerra.diaz@gmail.com>
2024-05-07 16:07:17 +0100

    Server main implementation

---
+++ src/ServerSide/ServerRopeGame.java
@@ -0,0 +1,66 @@
+package ServerSide;
+
+import Game.ServerConfigs;
+import static java.lang.System.out;
+
+/**
+ * This class implements the Server of RopeGame client-server architecture. It
+ * also implements initialization of the passive ententies.
+ */
+public class ServerRopeGame {
+
+    /**
+     * Initializes the Server of RopeGame Client/Server implementation
+     * <ul>
+     * <li>Contestants Bench: CB
+     * <li>Playground: PG
+     * <li>Referee site: RS
+     * <li>General Information Repository: GR
+     * </ul>
+     *
+     * @param args --help for more info
+     */
+    public static void main(String[] args) {
+        ServerCom scon = null, sconi;
+        ServiceProviderAgent sps;
+        InterfaceServer servInterface = null;
+
+        if (args.length == 1) {
+            switch (args[0]) {
+                case "CB":
+                    scon = new ServerCom(ServerConfigs.CONTESTANTS_BENCH_PORT);
+                    servInterface = new ContestantsBenchInterface();
+                    break;
+                case "PG":
+                    scon = new ServerCom(ServerConfigs.PLAYGROUND_PORT);
+                    servInterface = new PlaygroundInterface();
+                    break;
+                case "RS":
+                    scon = new ServerCom(ServerConfigs.REFEREE_SITE_PORT);
+                    servInterface = new RefereeSiteInterface();
+                    break;
+                case "GR":
+                    scon = new ServerCom(ServerConfigs.GENERAL_INFORMATION_REPOSITORY_PORT);
+                    servInterface = new GeneralInformationRepositoryInterface();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (servInterface == null || scon == null) {
+            System.out.println("Server didn't start - bad argument - [CB | PG | RS | GR]");
+            System.exit(1);
+        }
+
+        scon.start();
+        out.println("Server " + args[0] +" started and listening for connections...");
+
+        while (true) {
+            sconi = scon.accept();
+            sps = new ServiceProviderAgent(sconi, servInterface);
+            sps.start();
+        }
+    }
+
+}
---
+++ src/ServerSide/ServiceProviderAgent.java
@@ -0,0 +1,173 @@
+package ServerSide;
+
+import ClientSide.Contestant;
+import Communication.Message;
+import Communication.MessageException;
+import Interfaces.InterfaceCoach;
+import Interfaces.InterfaceCoach.CoachState;
+import Interfaces.InterfaceContestant;
+import Interfaces.InterfaceContestant.ContestantState;
+import Interfaces.InterfaceReferee;
+import Interfaces.InterfaceReferee.RefereeState;
+import static java.lang.System.out;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class implements all Coach, Contestant and Referee interfaces. The
+ * purpose is to serve the incoming messages and forward to the right passive
+ * class implementation of the InterfaceServer.
+ *
+ * @author Eduardo Sousa - eduardosousa@ua.pt
+ * @author Guilherme Cardoso - gjc@ua.pt
+ * @version 2016-2
+ */
+public class ServiceProviderAgent extends Thread implements InterfaceCoach,
+        InterfaceContestant,
+        InterfaceReferee,
+        Comparable<InterfaceContestant> {
+
+    private final ServerCom sconi;
+    private final InterfaceServer servInterface;
+
+    private static int serviceProviderAgentId = 0;  // spa initialisation counter
+
+    // referee personalization
+    private Enum state;
+
+    // coach personalization
+    private int team;
+
+    // contestant personalization
+    private int contestantId;
+    private int strength;
+
+    /**
+     * Initialisation of the server interface
+     *
+     * @param scon connection of the main server
+     * @param sconi connection accepted by the main server
+     * @param servInterface server interface to be provided
+     */
+    ServiceProviderAgent(ServerCom sconi,
+            InterfaceServer servInterface) {
+
+        super(Integer.toString(serviceProviderAgentId++));
+        this.sconi = sconi;
+        this.servInterface = servInterface;
+        this.state = null;
+        this.team = 0;
+        this.contestantId = 0;
+        this.strength = 0;
+    }
+
+    @Override
+    public void run() {
+        Message inMessage = null,
+                outMessage = null;
+
+        Thread.currentThread().setName("SPA-" + Integer.toString(serviceProviderAgentId));
+
+        inMessage = (Message) sconi.readObject();
+
+        out.println(Thread.currentThread().getName() + ": " + inMessage.getType());
+
+        if (inMessage.getType() == Message.MessageType.SHUTDOWN) {
+            boolean shutdown = servInterface.goingToShutdown();
+
+            outMessage = new Message(Message.MessageType.OK);
+
+            sconi.writeObject(outMessage);
+            sconi.close();
+
+            if (shutdown) {
+                System.exit(0);
+            }
+        } else {
+            // TODO: validate message
+            this.state = inMessage.getState();
+            this.team = inMessage.getTeam();
+            this.contestantId = inMessage.getContestantId();
+            this.strength = inMessage.getStrength();
+
+            try {
+                outMessage = servInterface.processAndReply(inMessage);
+            } catch (MessageException ex) {
+                Logger.getLogger(ServiceProviderAgent.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            sconi.writeObject(outMessage);
+            sconi.close();
+        }
+
+    }
+
+    // Coach methods
+    @Override
+    public CoachState getCoachState() {
+        return (CoachState) state;
+    }
+
+    @Override
+    public void setCoachState(CoachState state) {
+        this.state = state;
+    }
+
+    @Override
+    public int getTeam() {
+        return team;
+    }
+
+    @Override
+    public void setTeam(int team) {
+        this.team = team;
+    }
+
+    // Contestant methods
+    @Override
+    public int getContestantId() {
+        return contestantId;
+    }
+
+    @Override
+    public void setContestantId(int id) {
+        contestantId = id;
+    }
+
+    @Override
+    public ContestantState getContestantState() {
+        return (ContestantState) state;
+    }
+
+    @Override
+    public void setContestantState(Contestant.ContestantState state) {
+        this.state = state;
+    }
+
+    @Override
+    public int getStrength() {
+        return strength;
+    }
+
+    @Override
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    // Referee methods
+    @Override
+    public RefereeState getRefereeState() {
+        return (RefereeState) state;
+    }
+
+    @Override
+    public void setRefereeState(RefereeState state) {
+        this.state = state;
+    }
+
+    @Override
+    public int compareTo(InterfaceContestant contestant) {
+        return getContestantId() - contestant.getContestantId();
+    }
+
+}
