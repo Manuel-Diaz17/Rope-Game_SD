d710859aea808d3a2e03139acb4487c66a2f3ede
Manuel Diaz <manu.guerra.diaz@gmail.com>
2024-05-07 16:00:36 +0100

    implementation of the interfaces on the ServerSide

---
+++ src/ServerSide/InterfaceServer.java
@@ -0,0 +1,29 @@
+package ServerSide;
+
+import Communication.Message;
+import Communication.MessageException;
+
+/**
+ * Interface that defines the operations available passive classes that are
+ * being served through messages
+ */
+interface InterfaceServer {
+
+    /**
+     * Process the message and reply accordingly to the operation done in the
+     * passive class
+     *
+     * @param inMessage message to be decoded and forwarded to the right method
+     * @return message that will be answered
+     * @throws message exception if message doesn't fit in the initialised
+     * interface
+     */
+    Message processAndReply(Message inMessage) throws MessageException;
+
+    /**
+     * Checks if the interface has ended operations and is going to shutdown
+     *
+     * @return true if going to shutdown
+     */
+    boolean goingToShutdown();
+}
